dfe4e8468856330530fa487481750b0c705bb091
sky leilani <45158875+skyleilani@users.noreply.github.com>
2023-01-05 18:47:45 -0800

    Update useFetchData.tsx

--- useFetchData.tsx
+++ useFetchData.tsx
@@ -1,28 +1,44 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
  //a simple custom hook that fetches data from an API and stores it in a state variable
  
 const useFetchData = (url: string) => {
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
+ 
+ //initialize state variables for data, loading, and error
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  //fetch data on url change
   useEffect(() => {
+   
+   // fetchData is an async function to fetch data 
     const fetchData = async () => {
       try {
-        const response = await fetch(url)
-        const data = await response.json()
-        setData(data)
+       //make api request
+        const response = await fetch(url);
+        
+        //parse response as JSON
+        const data = await response.json();
+        
+        //update the data state with parsed response
+        setData(data);
       } catch (err) {
-        setError(err)
+       
+       //set error state if there is an error
+        setError(err);
       } finally {
-        setLoading(false)
+       
+       // set loading state to false
+        setLoading(false);
       }
     }
-    fetchData()
-  }, [url])
+    
+    // call fetchData to initiate API request
+    fetchData();
+  }, [url]); // only refetch data when url changes
 
   return { data, loading, error }
-}
+};
 
-export default useFetchData
+export default useFetchData;
